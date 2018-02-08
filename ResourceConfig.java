b7af880298f2849e9daacf93f1aab78c32cd69a1
parameme123 <33595961+parameme123@users.noreply.github.com>
2018-02-08 23:44:16 +0000

    Add files via upload

--- ResourceConfig.java
+++ ResourceConfig.java
@@ -7,16 +7,11 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 public class ResourceConfig extends WebMvcConfigurerAdapter {
-
 	@Configuration
 	public class ResourcesConfig extends WebMvcConfigurerAdapter {
-
 		public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
 			registry.addResourceHandler(new String[] { "/**" }).addResourceLocations(new String[] { "/" })
 					.setCacheControl(CacheControl.maxAge(0, TimeUnit.MILLISECONDS));
-
 		}
-
 	}
 }
