4a5c77671cb2819c8a7db01c929e06f6edd06806
parameme123 <33595961+parameme123@users.noreply.github.com>
2018-02-08 23:43:22 +0000

    Add files via upload

--- UserToken.java
+++ UserToken.java
@@ -3,14 +3,11 @@ package FileEncr;
 public class UserToken {
 
 	private static String token;
-
+	
 	public static String getToken() {
 		return token;
 	}
-
 	public static void setToken(String token) {
 		UserToken.token = token;
 	}
-	
-	
 }
